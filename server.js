24e8df7822656328d5a217322c78f723c4395c4b
pelasg0 <91074931+pelasg0@users.noreply.github.com>
2026-01-11 23:43:57 +0100

    cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 const COUNT_FILE = path.join(__dirname, 'count.txt');
 
 // Middleware
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
